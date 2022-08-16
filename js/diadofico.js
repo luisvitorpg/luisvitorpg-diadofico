d23a076ecbc5e178c09298a73ebeafa8700fb34a
luisvitorpg <99150383+luisvitorpg@users.noreply.github.com>
2022-08-16 20:22:11 -0300

    Add files via upload

---
+++ js/diadofico.js
@@ -0,0 +1,19 @@
+function iniciar() {
+    window.location = 'pagina1.html';
+}
+
+function ContinuarDaPagina1() {
+    window.location = 'pagina2.html';
+}
+
+function continuarDaPagina2() {
+    window.location = 'pagina3.html';
+}
+
+function continuarDaPagina3() {
+    window.location = 'pagina4.html';
+}
+
+function terminar() {
+    window.location = 'fim.html';
+}
